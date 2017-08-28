a19832edf19348c2f8d3d83ef8cd4e86f21190ea
akhilsinghal1234 <akhilsinghal1234@gmail.com>
2017-08-28 15:58:35 +0000

    modified

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect('localhost','root','') or die();
-$db = mysqli_select_db($con,'arduino') or die();
+$con = mysqli_connect('localhost','root','password') or die();
+$db = mysqli_select_db($con,'db_name') or die();
 ?>
